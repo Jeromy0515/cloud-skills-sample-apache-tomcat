4351550990cd8380fcdb612eb943122cc0a9413b
ParkMinJae <77256585+Jeromy0515@users.noreply.github.com>
2022-04-18 14:24:51 +0900

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -12,7 +12,7 @@ tar -zvxf apache-tomcat-9.0.60.tar.gz
 aws s3 cp s3://<bucket-name>/<file-name>.war /
 mv /<file-name>.war /ROOT.war
 
-rm -rf /apache-tomcat-9.0.60/webapps/ROOT
+rm -rf /apache-tomcat-9.0.60/webapps/*
 mv /ROOT.war /apache-tomcat-9.0.60/webapps/
 
 /apache-tomcat-9.0.60/bin/startup.sh
